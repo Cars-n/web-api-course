cfc119af3ec61a2bd91195bba0b1c182d03b4e61
Cars-n <carsonsmith8@hotmail.com>
2025-04-08 20:55:07 +0000

    add day 2 code

--- src/SoftwareCenterSolution/SoftwareCenter.Api/Program.cs
+++ src/SoftwareCenterSolution/SoftwareCenter.Api/Program.cs
@@ -1,10 +1,38 @@
+using FluentValidation;
+using Marten;
+using Microsoft.AspNetCore.Http.Json;
+using SoftwareCenter.Api.Vendors;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("software") ?? throw new Exception("No connection string! Don't start me");
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(connectionString);
+}).UseLightweightSessions(); // not worth explaining, Marten specific, unless you are interested ask me later.
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+// global (other than controllers) for minimal APIs, or for instances of the JSON Serializer like you'll see later.
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.DictionaryKeyPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
+    // this is useful for errors with validation
+    // enums
+    options.SerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
+});
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.DictionaryKeyPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
+    options.JsonSerializerOptions.DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// scoped means a new instance of this thing per http request.
+builder.Services.AddScoped<IValidator<CommercialVendorCreate>, CommercialVendorCreateValidator>();
 
 var app = builder.Build();
 
@@ -12,6 +40,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Software Center");
+    });
 }
 
 app.UseAuthorization();
@@ -20,4 +52,4 @@ app.MapControllers();
 
 app.Run();
 
-public partial class Program { };
+public partial class Program { }
--- src/SoftwareCenterSolution/SoftwareCenter.Tests/Vendors/AddingAVendor.cs
+++ src/SoftwareCenterSolution/SoftwareCenter.Tests/Vendors/AddingAVendor.cs
@@ -1,26 +1,77 @@
-﻿using System.Net.Http.Json;
+﻿
+
+using System.Net.Http.Json;
 using Alba;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+using SoftwareCenter.Api.Vendors;
 
 namespace SoftwareCenter.Tests.Vendors;
+
 public class AddingAVendor
 {
-    [Fact]
-    public async Task CanAddAVendorAsync()
-    {
-        var host = await AlbaHost.For<Program>();
 
-        // Syatem Tests are "scenarios"
-        await host.Scenario(api =>
+    [Fact]
+    public async Task CanAddVendorAsync()
+    {
+
+        var host = await AlbaHost.For<Program>(config =>
         {
-            api.Post.Json(new { }).ToUrl("/vendors");
+            var fakeIdentity = Substitute.For<IProvideIdentity>();
+            fakeIdentity.GetNameOfCallerAsync().Returns("darth-vader");
+            config.ConfigureServices(services =>
+            {
+                services.AddScoped<IProvideIdentity>(_ => fakeIdentity);
+            });
+        });
+
+        var vendorToAdd = new CommercialVendorCreateModel
+        {
+            Name = "Oracle",
+            Site = "https://oracle.com",
+            ContactEmail = "larry@oracle.com",
+            ContactFirstName = "Larry",
+            ContactLastName = "Ellison",
+            ContactPhone = "800 999-9999"
+        };
+
+        // System Tests are "scenarios".  
+        var response = await host.Scenario(api =>
+        {
+            api.Post.Json(vendorToAdd).ToUrl("/commercial-vendors");
+            api.StatusCodeShouldBe(201);
+        });
+
+        var responseBody = response.ReadAsJson<VendorEntity>();
+
+        Assert.NotNull(responseBody);
+
+        Assert.Equal(vendorToAdd.Name, responseBody.Name);
+        // etc. etc. BORING..
+
+        var location = response.Context.Response.Headers.Location.Single();
+
+        Assert.NotNull(location);
+
+        var getResponse = await host.Scenario(api =>
+        {
+            api.Get.Url(location);
             api.StatusCodeShouldBeOk();
         });
 
-        var client = new HttpClient();
-        client.BaseAddress = new Uri("http://localhost:1337");
+        var getResponseBody = getResponse.ReadAsJson<VendorEntity>();
+        Assert.NotNull(getResponseBody);
 
-        var response = await client.PostAsJsonAsync("vendors", new { });
-
-        Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        //Assert.Equal(responseBody, getResponseBody);
+        Assert.Equivalent(responseBody, getResponseBody);
     }
 }
+
+
+//public class FakeIdentityThing : IProvideIdentity
+//{
+//    public Task<string> GetNameOfCallerAsync()
+//    {
+//        return Task.FromResult("boba-fett");
+//    }
+//}
